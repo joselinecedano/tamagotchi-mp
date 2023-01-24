4183e679d2581c8300b47883b7d6b2868232e46d
joselinecedano <joselinecedano0@gmail.com>
2023-01-23 19:55:08 -0500

    Functions and prompt work

--- js/app.js
+++ js/app.js
@@ -2,32 +2,41 @@ console.log("Tamagotchi Mini Project")
 
 //link all of my buttons to their variable that i will be using to alter them with 
 const hungerBar = document.querySelector("#hunger")
-console.log(hungerBar)
-const energyBar = document.querySelector("#energy")
-console.log(energyBar)
+const sleepBar = document.querySelector("#sleepiness")
 const boredBar = document.querySelector("#boredom")
-console.log(boredBar)
 const dayOrNight = document.querySelector("#gameArea")
-console.log(dayOrNight) 
 const resetBtn = document.querySelector(".resetBtn")
-console.log(resetBtn)
 const eatBtn = document.querySelector(".hungry")
-console.log(eatBtn)
 const sleepBtn = document.querySelector(".sleepy")
-console.log(sleepBtn)
 const playBtn = document.querySelector(".bored")
-console.log(playBtn)
 const infoBtn = document.querySelector(".open-instruction")
-console.log(infoBtn)
 const closeInfo = document.querySelector(".close-instructions")
-console.log(closeInfo)
 
 //set up prompt for user to input their tama name and update it inside of the game!
 const mainPrompt = () => {
-    let tamaName = prompt ("Welcome to the world of Tamagotchi! What would you like to name your new friend?","")
-    document.querySelector(".tamagotchiName").innerHTML= tamaName;
-
+    let promptName= prompt ("Welcome to the world of Tamagotchi! What would you like to name your new friend?","")
+    document.querySelector(".tamagotchiName").innerHTML= promptName;
+    return promptName
 }
+
+
+//set up class for future pets
+class Tamagotchi{
+    constructor(name, hunger,sleepiness,boredom, age){
+        this.name = name = mainPrompt()
+        this.hunger = hunger //= hungerBar.value
+        this.sleepiness = sleepiness //= sleepBar.value
+        this.boredom = boredom //= boredBar.value
+        this.age = age
+        console.log(this.hunger)
+    }  
+}
+const tamatomo = new Tamagotchi ("",1 ,1 ,1 ,0)
+console.log(tamatomo)
+
+// let tama = []
+// let ageLevel = 
+
 //open info button
 function openInfo() {
     document.getElementById("info-popup").style.visibility = "visible"
@@ -40,23 +49,20 @@ const closePopup = () => {
 }
 closeInfo.addEventListener("click", closePopup)
 
-//allow player to feed their tamagotchi, play with their tamagotchu, and make their tamagotchi sleep
-// Hunger -10  Energy -5  Boredom +10 //For Feed action
-// Energy +10 Hunger +10 Boredom +5 //Sleep action
-// Bordemon -10 Hunger +15 Energy -15 //Play actio 
 
+//allow player to feed their tamagotchi, play with their tamagotchu, and make their tamagotchi sleep
+// Hunger -3  Sleepiness +2  Boredom +1 //For Feed action
+// Sleepiness -3 Hunger +2 Boredom +1 //Sleep action
+// Bordemon -2 Hunger +3 Sleepiness +1 //Play action 
 //feed tamagotchi function
-const feedTama = () => {
+    const feedTama = ()=> {
     dayOrNight.setAttribute("class","lightMode")
     if(hungerBar.value >= 0){
-    hungerBar.value -=10
-    energyBar.value -= 5
-    boredBar.value += 10
-    }else if (boredBar.value == 100){
-        alert(`${tamaName} died of boredom!`)
-        resetGame()
-    }else if (energyBar.value == 100){
-        alert(`${tamaName} died from lack of energy!`)
+    hungerBar.value -=3
+    sleepBar.value += 2
+    boredBar.value += 1
+   } if(boredBar.value == 10 || sleepBar.value == 10 || hungerBar.value == 10){
+       alert(`Your little buddy is dead! Do better! Your buddies stats: hunger = ${hungerBar.value}, sleepiness = ${sleepBar.value}, and boredom = ${boredBar.value}.`)
         resetGame()
     }
 }
@@ -65,15 +71,12 @@ eatBtn.addEventListener("click",feedTama)
 //put tamagotchi to sleep function
 const sleepyTama = () => {
 dayOrNight.setAttribute("class", "darkMode")
-if(energyBar.value > 0){
-    energyBar.value +=10
-    hungerBar.value += 10
-    boredBar.value += 10
-    }else if(hungerBar.value == 100){
-        alert(`${tamaName} died of hunger!`)
-        resetGame()
-    }else if(boredBar.value == 100){
-        alert(`${tamaName} died of boredom!`)
+if(sleepBar.value >= 0){
+    sleepBar.value -= 3
+    hungerBar.value += 2
+    boredBar.value += 1
+    }if(hungerBar.value == 10 || boredBar.value == 10 || sleepBar.value == 10 ){
+        alert(`Your little buddy ran away! Do better! Your buddies stats: hunger = ${hungerBar.value}, sleepiness = ${sleepBar.value}, and boredom = ${boredBar.value}.`)
         resetGame()
     }
 }
@@ -83,15 +86,11 @@ sleepBtn.addEventListener("click", sleepyTama)
 const playTama = () => {
     dayOrNight.setAttribute("class","lightMode")
     if (boredBar.value >= 0){
-        boredBar.value -=10
-        hungerBar.value += 10
-        energyBar.value -=15
-    console.log(energyBar.value)
-    } else if (energyBar.value == 0){
-        alert(`${tamaName} died from lack of energy!`)
-        resetGame()
-    }else if (hungerBar.value == 0){
-        alert(`${tamaName} died of hunger!`)
+        boredBar.value -=1
+        hungerBar.value += 1
+        sleepBar.value +=2
+    }if(hungerBar.value == 10 || sleepBar.value == 10 || boredBar.value == 10){
+        alert(`Your little buddy decided to find a better parent! Do better! Your buddies stats: hunger = ${hungerBar.value}, sleepiness = ${sleepBar.value}, and boredom = ${boredBar.value}.`)
         resetGame()
     }
 }
@@ -99,15 +98,14 @@ playBtn.addEventListener("click", playTama)
 
 //reset the stats
 const resetStats = () => {
-hungerBar.value = 10;
-energyBar.value= 100;
-boredBar.value= 20;
+hungerBar.value = 1;
+sleepBar.value= 1;
+boredBar.value= 1;
 }
 //reset the game
 const resetGame = () => {
-    resetStats()
-    mainPrompt()
-    console.log("Game Reset", "Stats are " +hungerBar.value + ","+ energyBar.value +","+ boredBar.value+ "!")
+        resetStats()
+        mainPrompt()
 }
 resetBtn.addEventListener("click", resetGame)
 
